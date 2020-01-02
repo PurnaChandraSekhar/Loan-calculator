ce08330df793fb34d9127f398b982a05aae1ba57
Chandu <chanduchan225@gmail.com>
2020-01-02 20:55:52 +0530

    third commit added interactivity

---
+++ app.js
@@ -0,0 +1,57 @@
+//Listen for submit event
+document.querySelector('#form').addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    //intially hide results
+    document.querySelector('.container-2').style.display = 'none';
+    setTimeout(calculateResults, 0);
+    //now show results after 0 sec
+    document.querySelector('.container-2').style.display = 'flex';
+
+});
+
+//initially hide the results
+
+
+//calculate result
+function calculateResults() {
+  // input variables
+    let loanAmount = document.querySelector('#loan-amount');
+    let interestAmount = document.querySelector('#interest-amount');
+    let payment = document.querySelector('#payment');
+    let monthlyPayment = document.querySelector('#monthly');
+    let interest = document.querySelector('#interest');
+    let totalPayment = document.querySelector('#total-payment');
+
+    let principal = parseFloat(loanAmount.value);
+    let calculatedInterest = parseFloat(interestAmount.value) / 100 / 12;
+    let calculatedPayments = parseFloat(payment.value) * 12;
+
+    //compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/ (x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        interest.value = (monthly*calculatedPayments).toFixed(2);
+        totalPayment.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        errorMessage("Please check your numbers");
+    }
+}
+
+//error message
+function errorMessage(msg) {
+    const div = document.createElement('div');
+    const container = document.querySelector('#form');
+    const insertbefore = document.querySelector('#container-em');
+    div.className = "error-msg";
+    div.innerHTML = `<p>${msg}</p>`
+ 
+    container.insertBefore(div, insertbefore);
+    //clear error
+    setTimeout(() => {
+      const el = document.querySelector('.error-msg');
+       el.remove();
+    }, 3000);
+}
