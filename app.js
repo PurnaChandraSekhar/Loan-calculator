8b2223b98b4b07d29c8846fbc85871132cd5c7bc
Chandu <chanduchan225@gmail.com>
2020-01-04 22:13:55 +0530

    error solved

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
 //Listen for submit event
 document.querySelector('#form').addEventListener('submit', function(e){
     e.preventDefault();
+
+     //intially hide results
+     document.querySelector('.container-2').style.display = 'none';
     
-    //intially hide results
-    document.querySelector('.container-2').style.display = 'none';
-    calculateResults();
+    //check values
+    let loanAmount = document.querySelector('#loan-amount').value;
+    let interestAmount = document.querySelector('#interest-amount').value;
+    let payment = document.querySelector('#payment').value;
+    
+     if(loanAmount <= 0 || interestAmount <= 0 || payment <=0) {
+        errorMessage("please check your input");
+        return;
+     }
 
-});
-
-//initially hide the results
-
-
-//calculate result
-function calculateResults() {
-  // input variables
-    let loanAmount = document.querySelector('#loan-amount');
-    let interestAmount = document.querySelector('#interest-amount');
-    let payment = document.querySelector('#payment');
+    // output variables
     let monthlyPayment = document.querySelector('#monthly');
     let interest = document.querySelector('#interest');
     let totalPayment = document.querySelector('#total-payment');
+ 
+     //calculate result
+     let principal = parseFloat(loanAmount);
+     let calculatedInterest = parseFloat(interestAmount) / 100 / 12;
+     let calculatedPayments = parseFloat(payment) * 12;
 
-    let principal = parseFloat(loanAmount.value);
-    let calculatedInterest = parseFloat(interestAmount.value) / 100 / 12;
-    let calculatedPayments = parseFloat(payment.value) * 12;
+     
+     //compute monthly payment
+     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+     const monthly = (principal*x*calculatedInterest)/ (x-1);
 
-    //compute monthly payment
-    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal*x*calculatedInterest)/ (x-1);
-
-    if(isFinite(monthly)) {
-        monthlyPayment.value = monthly.toFixed(2);
-        interest.value = (monthly*calculatedPayments).toFixed(2);
-        totalPayment.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+     if(isFinite(monthly)) {
+         monthlyPayment.value = monthly.toFixed(2);
+         interest.value = (monthly*calculatedPayments).toFixed(2);
+         totalPayment.value = ((monthly * calculatedPayments) - principal).toFixed(2);
 
          //now show results after 0 sec
          document.querySelector('.container-2').style.display = 'flex';
-    } else {
-        errorMessage("Please check your numbers");
+     } else {
+         errorMessage("Please check your numbers");
          
-    }
-}
+     }
+});
 
 //error message
 function errorMessage(msg) {
